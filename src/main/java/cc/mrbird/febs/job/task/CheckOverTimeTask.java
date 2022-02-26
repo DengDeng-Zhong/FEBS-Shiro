182eff05a6fdeed13de5053f78fcf6be3e4a9777
dengdeng-zhong <862283389@qq.com>
2022-02-26 23:01:27 +0800

    增加一个job

---
+++ src/main/java/cc/mrbird/febs/job/task/CheckOverTimeTask.java
@@ -0,0 +1,12 @@
+package cc.mrbird.febs.job.task;
+
+/**
+ * 检查即将超时的任务
+ *
+ * @author Zhong-Dengbo
+ * @version V1.0
+ * @ClassName: CheckOverTimeTask
+ * @date 2022/2/26 23:00
+ */
+public class CheckOverTimeTask {
+}
